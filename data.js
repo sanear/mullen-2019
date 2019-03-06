5a6b58c77f4165a4ce31e2c8cd2fea91d3db9d10
Andrew Sannier <andrew.sannier@oracle.com>
2019-03-06 15:30:47 -0700

    Housekeeping on index + basic global data object

---
+++ data.js
@@ -0,0 +1,10 @@
+var restaurants = [
+  {
+    "name": "Chick Fil-A",
+    "locationName": "River Point",
+    "address": "4090 River Point Pkwy, Sheridan, CO 80110",
+    "id": 0,
+    "rating": 5,
+    "priceLevel": 2
+  }
+]
