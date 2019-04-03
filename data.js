510781e33e533aac81f21cba1726995e86a1d332
KexinCoco <ykxcoco@163.com>
2019-04-03 15:57:45 -0600

    add two resteraunts
    Add Panera and Costco

--- data.js
+++ data.js
@@ -3,8 +3,23 @@ var restaurants = [
     "name": "Chick Fil-A",
     "locationName": "River Point",
     "address": "4090 River Point Pkwy, Sheridan, CO 80110",
-    "id": 0,
     "rating": 5,
+    "priceLevel": 2
+  },
+ 
+  {
+    "name": "Panera Bread",
+    "locationName": "River Point",
+    "address": "3702 River Point Pkwy, Englewood, CO 80110",
+    "rating": 3.7,
+    "priceLevel": 2
+  },
+  
+  {
+    "name": "Costco",
+    "locationName": "River Point",
+    "address": "4000 River Point Pkwy, Sheridan, CO 80110",
+    "rating": 4.5,
     "priceLevel": 2
   }
 ]
