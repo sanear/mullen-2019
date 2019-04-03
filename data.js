32e711b6b659415d93d7393f39eb0b89515e9d83
angelina0704 <48299799+angelina0704@users.noreply.github.com>
2019-04-03 15:59:49 -0600

    Add resteraunts Noodles and Chipotle

--- data.js
+++ data.js
@@ -3,8 +3,22 @@ var restaurants = [
     "name": "Chick Fil-A",
     "locationName": "River Point",
     "address": "4090 River Point Pkwy, Sheridan, CO 80110",
-    "id": 0,
     "rating": 5,
     "priceLevel": 2
+  },
+  {
+    "name": "Noodles & Co.",
+    "locationName": "Hampden",
+    "address": "697 W Hampden Ave, Englewood, CO 80110",
+    "rating": 4,
+    "priceLevel": 1
+  },
+  {
+    "name": "Chipotle",
+    "locationName": "Hampden",
+    "address": "333 W Hampden Ave Ste 110, Englewood, CO 80110",
+    "rating": 4,
+    "priceLevel": 1
   }
+  
 ]
