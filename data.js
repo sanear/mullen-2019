10f39302188b26a9c955e1fe31fe430c140b86d6
Andrew Sannier <andrew.sannier@oracle.com>
2019-04-03 15:55:01 -0600

    Tiny change to button to make consistent, added my two restaurants

--- data.js
+++ data.js
@@ -3,8 +3,21 @@ var restaurants = [
     "name": "Chick Fil-A",
     "locationName": "River Point",
     "address": "4090 River Point Pkwy, Sheridan, CO 80110",
-    "id": 0,
     "rating": 5,
     "priceLevel": 2
+  },
+  {
+    "name": "Panda Express",
+    "locationName": "Englewood",
+    "address": "2990 S Broadway, Englewood, CO 80113",
+    "rating": 2,
+    "priceLevel": 3
+  },
+  {
+    "name": "Tokyo Joe's",
+    "locationName": "Hampden",
+    "address": "901 W Hampden Ave, Englewood, CO 80110",
+    "rating": 2,
+    "priceLevel": 3
   }
 ]
