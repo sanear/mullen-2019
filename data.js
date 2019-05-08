18b102402ef3bb0a838d369353046c99dabac2ad
Ash <ashley.blondo@mustangs>
2019-05-08 16:08:50 -0600

    Added images

--- data.js
+++ data.js
@@ -24,6 +24,7 @@ var restaurants = [
   },
   {
     "name": "Noodles & Co.",
+	"image": "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/112014/noodlesco_0_0.png?itok=SG63s9a5"
     "locationName": "Hampden",
     "address": "697 W Hampden Ave, Englewood, CO 80110",
     "rating": 4,
@@ -31,6 +32,7 @@ var restaurants = [
   },
   {
     "name": "Chipotle",
+	"image": "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Chipotle_Mexican_Grill_logo.svg/1200px-Chipotle_Mexican_Grill_logo.svg.png"
     "locationName": "Hampden",
     "address": "333 W Hampden Ave Ste 110, Englewood, CO 80110",
     "rating": 4,
@@ -38,6 +40,7 @@ var restaurants = [
   },
   {	
     "name": "Panda Express",
+	"image":"https://upload.wikimedia.org/wikipedia/en/thumb/8/85/Panda_Express_logo.svg/220px-Panda_Express_logo.svg.png"
     "locationName": "Englewood",
     "address": "2990 S Broadway, Englewood, CO 80113",
     "rating": 2,
@@ -45,6 +48,7 @@ var restaurants = [
   },
   {
     "name": "Tokyo Joe's",
+	"image": "https://adc3ef35f321fe6e725a-fb8aac3b3bf42afe824f73b606f0aa4c.ssl.cf1.rackcdn.com/tenantlogos/18529.png"
     "locationName": "Hampton",
     "address": "901 W Hampden Ave, Englewood, CO 80110",
     "rating": 2,
