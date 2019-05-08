c1e4f5b12add834290ab3de583d8c3853cc9f386
HaileyForar <44071700+HaileyForar@users.noreply.github.com>
2019-05-08 16:07:53 -0600

    Update data.js

--- data.js
+++ data.js
@@ -10,6 +10,8 @@ var restaurants = [
   },
   {
     "name": "Panera Bread",
+    "image": "https://upload.wikimedia.org/wikipedia/en/thumb/c/cb/Panera_Bread_logo.svg/1200px-Panera_Bread_logo.svg.png",
+    "description": "THIS IS A DESCRIPTION",
     "locationName": "River Point",
     "address": "3702 River Point Pkwy, Englewood, CO 80110",
     "rating": 3.7,
@@ -17,6 +19,7 @@ var restaurants = [
   },
   {
     "name": "Costco",
+    "image": "https://i.ytimg.com/vi/PbIu4lAE3XI/maxresdefault.jpg",
     "locationName": "River Point",
     "address": "4000 River Point Pkwy, Sheridan, CO 80110",
     "rating": 4.5,
@@ -24,6 +27,7 @@ var restaurants = [
   },
   {
     "name": "Noodles & Co.",
+    "image": "https://jsmliving.com/uploads/images/JSM%20VIP/Noodles_Company.png",
     "locationName": "Hampden",
     "address": "697 W Hampden Ave, Englewood, CO 80110",
     "rating": 4,
@@ -31,6 +35,7 @@ var restaurants = [
   },
   {
     "name": "Chipotle",
+    "image": "https://vignette.wikia.nocookie.net/uncyclopedia/images/d/dd/Chipotle_logo.jpg/revision/latest?cb=20180222111048",
     "locationName": "Hampden",
     "address": "333 W Hampden Ave Ste 110, Englewood, CO 80110",
     "rating": 4,
@@ -38,6 +43,7 @@ var restaurants = [
   },
   {	
     "name": "Panda Express",
+    "image": "https://i.ytimg.com/vi/PbIu4lAE3XI/maxresdefault.jpg",
     "locationName": "Englewood",
     "address": "2990 S Broadway, Englewood, CO 80113",
     "rating": 2,
@@ -45,6 +51,7 @@ var restaurants = [
   },
   {
     "name": "Tokyo Joe's",
+    "image": "https://i.ytimg.com/vi/PbIu4lAE3XI/maxresdefault.jpg",
     "locationName": "Hampton",
     "address": "901 W Hampden Ave, Englewood, CO 80110",
     "rating": 2,
