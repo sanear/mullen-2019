9c530c5eee345916c14dc91917fcccf554a71e17
Ash <ashley.blondo@mustangs>
2019-05-08 16:46:03 -0600

    Added places

--- data.js
+++ data.js
@@ -35,7 +35,7 @@ var restaurants = [
   },
   {
     "name": "Chipotle",
-	"image": "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Chipotle_Mexican_Grill_logo.svg/1200px-Chipotle_Mexican_Grill_logo.svg.png"
+	"image": "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Chipotle_Mexican_Grill_logo.svg/1200px-Chipotle_Mexican_Grill_logo.svg.png",
     "locationName": "Hampden",
     "address": "333 W Hampden Ave Ste 110, Englewood, CO 80110",
     "rating": 4,
@@ -51,10 +51,42 @@ var restaurants = [
   },
   {
     "name": "Tokyo Joe's",
-	"image": "https://adc3ef35f321fe6e725a-fb8aac3b3bf42afe824f73b606f0aa4c.ssl.cf1.rackcdn.com/tenantlogos/18529.png"
+	"image": "https://adc3ef35f321fe6e725a-fb8aac3b3bf42afe824f73b606f0aa4c.ssl.cf1.rackcdn.com/tenantlogos/18529.png",
     "locationName": "Hampton",
     "address": "901 W Hampden Ave, Englewood, CO 80110",
     "rating": 2,
     "priceLevel": 3
+  }
+  {
+	"name": "Starbucks"
+	"image": "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png",
+	"locationName": "River Point",
+	"address": "3500 A River Point Pkwy, Denver, CO 80110",
+	"rating": 3.9, 
+	"priceLevel": 3, 
+  }
+  {
+	"name": "McDonald's"
+	"image": "https://missionmarketplaceoceanside.com/wp-content/uploads/2018/06/mcdonalds-logo.jpg",
+	"locationName": "Federal",
+	"address": "3601 S Federal Blvd, Englewood, CO 80110",
+	"rating": 3.3,
+	"priceLevel": 1,
+  }
+  {
+	"name": "Taco Bell"
+	"image": "https://brandstruck.co/wp-content/uploads/2018/05/taco-bell-brand-strategy.png",
+	"locationName": "South Sheradin",
+	"address": "3110 South Sheridan, Denver, CO 80227",
+	"rating": 3.2,
+	"priceLevel": 1,
+  }
+  {
+	"name": "Steak and Shake"
+	"image": "https://vignette.wikia.nocookie.net/logopedia/images/a/a9/Shake.jpg/revision/latest?cb=20110921154140",
+	"locationName": "River Point",
+	"address": "3502 River Point Pkwy, Sheridan, CO 80110",
+	"rating": 3.5,
+	"priceLevel": 2,
   }
 ];
